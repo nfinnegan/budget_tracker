872222cf22cd603b89dc216295ef9f55fdfcf1ad
Natalie Finnegan <nfinneg2@gmail.com>
2021-06-10 15:18:27 -0600

    add files to cache, + two funcs for idx check/add

--- public/db.js
+++ public/db.js
@@ -21,6 +21,51 @@ request.onupgradeneeded = (ev) => {
   }
 };
 
+//checking indexeddb to see if there are any values to be added to main db once network is back online
+const checkDatabase = () => {
+  console.log("check db invoked");
+
+  //Open a transaction to the BudgetStore
+  let transaction = db.transaction(["BudgetStore"], "readwrite");
+
+  // access your BudgetStore object
+
+  const store = transaction.objectStore("BudgetStore");
+
+  //Get All records from store and set to a variable
+  const getAll = store.getAll();
+
+  //if the request was successful
+  getAll.onsuccess = function () {
+    //if there are items in the store, we need to bulk add them when we are back online
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.results),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          //if our returned response is not empty
+          if (res.length !== 0) {
+            //Open another transaction to BudgetStore with the ability to read and write
+            transaction = db.transaction(["BudgetStore"], "readwrite");
+
+            //Assign the current store to the variable
+            const currentStore = transaction.objectStore("BudgetStore");
+
+            //Clear the existing entries because our bulk add was successful
+            currentStore.clear();
+            console.log("Clearing store");
+          }
+        });
+    }
+  };
+};
+
 //on success console log the result
 request.onsuccess = (ev) => {
   console.log("success");
@@ -34,6 +79,22 @@ request.onsuccess = (ev) => {
   }
 };
 
+const saveRecord = (record) => {
+  console.log("record saved");
+  //create a transaction in the BudgetStore db with readwrite access
+  const transaction = db.transaction(["BudgetStore"], "readwrite");
+
+  //access the BudgetStore
+  const store = transaction.objectStore("BudgetStore");
+
+  //add record to your store with add method
+  store.add(record);
+};
+
+//error handling
 request.onerror = (ev) => {
   console.log(`Woops! ${ev.target.errorCode}`);
 };
+
+// Listen for app coming back online
+window.addEventListener("online", checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,8 @@ const FILES_TO_CACHE = [
   "/styles.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
-  "db.js",
+  "/db.js",
+  "/manifest.webmanifest",
 ];
 
 const CACHE_NAME = "static-cache-v1";
