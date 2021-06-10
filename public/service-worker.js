85eaf13711641c1645dbced2ef296d1765d47e38
Natalie Finnegan <nfinneg2@gmail.com>
2021-06-10 14:38:09 -0600

    added fetch to service wrkr

--- public/service-worker.js
+++ public/service-worker.js
@@ -22,6 +22,8 @@ self.addEventListener("install", (ev) => {
     })
   );
 
+  // tell the browser to activate this service worker immediately once it
+  // has finished installing
   self.skipWaiting();
 });
 
@@ -32,6 +34,7 @@ self.addEventListener("activate", (ev) => {
     caches
       .keys()
       .then((cacheNames) => {
+        //filters through array of old cache names to delete
         return cacheNames.filter(
           (cacheName) => !currentCaches.includes(cacheName)
         );
@@ -44,5 +47,42 @@ self.addEventListener("activate", (ev) => {
         );
       })
       .then(() => self.clients.claim())
+  );
+});
+
+//fetch
+self.addEventListener("fetch", (ev) => {
+  //cache successful requests to the API
+  if (ev.request.url.includes("/api/")) {
+    ev.respondWith(
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(ev.request)
+          .then((response) => {
+            // If the response was good, clone it and store it in the cache.
+
+            if (response.status === 200) {
+              cache.put(ev.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch((err) => {
+            //Network request failed, try to get it from the cache
+            return cache.match(ev.request);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+    );
+    return;
+  }
+
+  //if the request is not for the API, serve static assets using 'offline-first' approach.
+  ev.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(ev.request).then((response) => {
+        return response || fetch(ev.request);
+      });
+    })
   );
 });
